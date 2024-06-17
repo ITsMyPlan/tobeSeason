a62789096eee2fe6b3e6f3771f47803feea4ade0
imjurney <jurneyx2@gmail.com>
2024-06-17 16:57:56 +0900

    style:필요없는 스타일 제거

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import './App.css'
-import { Button } from './components/common/Button'
 import TodoList from './components/common/TodoList'
 
 function App() {
   return (
-    <>
-      <div className="bg-red-400">
         <TodoList />
-        <Button>Click me</Button>
-      </div>
-    </>
   )
 }
 
